360f7adf23009d7e048e9c1a426b4f81cbbe4884
Julian Bustos <julian.m.bustos@gmail.com>
2025-08-12 10:06:18 -0400

    feat: Cache getSheetData function for improved performance

--- app/getGoogleSheetData.ts
+++ app/getGoogleSheetData.ts
@@ -2,26 +2,29 @@
 
 import { google } from "googleapis";
 import { googleAuth } from "./googleAuth";
+import { cache } from "react";
 
-export const getSheetData = async (spreadsheetId: string, range: string) => {
-  try {
-    const auth = await googleAuth();
-    const sheets = google.sheets({
-      auth,
-      version: "v4",
-    });
+export const getSheetData = cache(
+  async (spreadsheetId: string, range: string) => {
+    try {
+      const auth = await googleAuth();
+      const sheets = google.sheets({
+        auth,
+        version: "v4",
+      });
 
-    // console.log(sheets.spreadsheets.values.get({}));
-    const response = await sheets.spreadsheets.values.get({
-      spreadsheetId,
-      range,
-    });
+      // console.log(sheets.spreadsheets.values.get({}));
+      const response = await sheets.spreadsheets.values.get({
+        spreadsheetId,
+        range,
+      });
 
-    return response.data.values?.filter((row) => {
-      return row && row.some((cell) => cell && String(cell).trim() !== "");
-    });
-  } catch (error) {
-    console.error("The API returned an error:", error);
-    throw new Error("failed to get the data");
+      return response.data.values?.filter((row) => {
+        return row && row.some((cell) => cell && String(cell).trim() !== "");
+      });
+    } catch (error) {
+      console.error("The API returned an error:", error);
+      throw new Error("failed to get the data");
+    }
   }
-};
+);
